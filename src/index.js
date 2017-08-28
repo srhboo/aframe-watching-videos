75eb398175fd788c4144e8ff72bb8fe048c7ac7c
desaree <srhboo@gmail.com>
2017-08-28 16:00:52 -0400

    put in two videos

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import 'babel-polyfill';
 import {Entity, Scene} from 'aframe-react';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import settingVideo1 from './assets/videos/setting-shot-1.mp4';
+import fanVideo1 from './assets/videos/fan.mp4';
 
 class App extends React.Component {
   constructor(props) {
@@ -25,6 +27,8 @@ class App extends React.Component {
         <a-assets>
           <img id="groundTexture" src="https://cdn.aframe.io/a-painter/images/floor.jpg"/>
           <img id="skyTexture" src="https://cdn.aframe.io/a-painter/images/sky.jpg"/>
+          <video id="vid-setting1" src={settingVideo1} autoplay loop="true"></video>
+          <video id="vid-fan1" src={fanVideo1} autoplay loop="true"></video>
         </a-assets>
 
         <Entity primitive="a-plane" src="#groundTexture" rotation="-90 0 0" height="100" width="100"/>
@@ -38,10 +42,10 @@ class App extends React.Component {
           geometry={{primitive: 'box'}}
           material={{color: this.state.color, opacity: 0.6}}
           animation__rotate={{property: 'rotation', dur: 2000, loop: true, to: '360 360 360'}}
-          animation__scale={{property: 'scale', dir: 'alternate', dur: 100, loop: true, to: '1.1 1.1 1.1'}}
+          animation__scale={{property: 'scale', dir: 'alternate', dur: 1000, loop: true, to: '1.1 1.1 1.1'}}
           position={{x: 0, y: 1, z: -3}}
           events={{click: this.changeColor.bind(this)}}>
-          <Entity animation__scale={{property: 'scale', dir: 'alternate', dur: 100, loop: true, to: '2 2 2'}}
+          <Entity animation__scale={{property: 'scale', dir: 'alternate', dur: 1000, loop: true, to: '2 2 2'}}
                   geometry={{primitive: 'box', depth: 0.2, height: 0.2, width: 0.2}}
                   material={{color: '#24CAFF'}}/>
         </Entity>
@@ -49,6 +53,8 @@ class App extends React.Component {
         <Entity primitive="a-camera">
           <Entity primitive="a-cursor" animation__click={{property: 'scale', startEvents: 'click', from: '0.1 0.1 0.1', to: '1 1 1', dur: 150}}/>
         </Entity>
+        <Entity primitive="a-plane" src="#vid-setting1" position="1 4 -10" rotation="0 0 0" width="10" height="8"/>
+        <Entity primitive="a-plane" src="#vid-fan1" position="1 9 -7" rotation="60 0 0" width="10" height="8"/>
       </Scene>
     );
   }
